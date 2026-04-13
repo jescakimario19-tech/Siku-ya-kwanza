69b37e4f440e0ea9d8372c52b6467af07076bbe4
jescakimario19-tech <jescakimario19@gmail.com>
2026-04-14 01:52:41 +0300

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+print("=== CALCULATOR YA TSH ===")
+namba1 = int(input("Ingiza namba ya kwanza: "))
+alama = input(ingiza alama +, -, *. / : ")
+namba2 = int(input ("Ingiza namba ya pili: ")
+if alama== "+" :
+             jibu = namba1 + namba2
+print ("jibu ni : ",jibu)
+elf alama== "-":
+jibu = namba1 - namba2
+print ("jibu ni : "'jibu)
+       else:
+print ("Alama hiyo sijafundishwa bado")
