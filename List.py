5bf96371136341d1eccdc4762e0fbcd1504c41ea
jescakimario19-tech <jescakimario19@gmail.com>
2026-04-15 16:18:57 +0300

    Create List.py

---
+++ List.py
@@ -0,0 +1,26 @@
+print ("TO-DO LIST YA BOOM")
+matumizi = [] # Hii ndio list itakayo hifadhi matumizi yote
+while True:
+  print ("/n chagua namba:")
+  print (" 1. ongeza tumizi jipya ")
+  print (" 2. onyesha matumizi yote")
+  print (" 3. Hesabu ya jumla ya boom")
+  print (" 4. Toka")
+  chaguo = input ("Andika namba 1-4:")
+  if chaguo == "1":
+    jina = input ("Andika tumizi: mfano chakula: ")
+    kiasi = int(input ("kiasi Tsh:"))
+    matumizi.append("jina+ "-" + str(kiasi))
+    print ("umehifadhi!")
+                    elif chaguo == "2":
+    print ("/n --- MATUMIZI YAKO ---")
+    for tumizi in matumizi
+    print(tumizi)
+                    elif chaguo == "3":
+  
+   print ("Tutahesabu jumla baadaye")
+elif chaguo == "4":
+print ("kwaheri.Dhibiti boom yako!")
+break
+    else:
+print("chaguo sio sahihi mkuu")
